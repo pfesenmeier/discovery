f380c054a991fdb0ab669d73720ef1054fd6648f
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-06-29 21:33:42 -0400

    Updates setup; cleanup

--- open-browser.sh
+++ open-browser.sh
@@ -1,11 +1,12 @@
 # https://github.com/rust-lang/cargo/issues/7557#issuecomment-791320960
-# open_browser.sh
+# open-browser.sh
 #! /bin/bash
-#echo $1
+
 winPath="file://///wsl$//Ubuntu${1}"
-#echo ${winPath}
+
 #firefox
 #/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe $winPath
+
 #chrome
 /mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe $winPath
 

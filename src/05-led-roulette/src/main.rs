d29ff6df390a1a33d9f09c0076c9426cd108b9eb
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-06-29 21:50:37 -0400

    Upload correct code

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -14,7 +14,7 @@ fn main() -> ! {
     let mut rng = RNG::<WyRand, usize>::new(42);
 
     loop {
-            let next = rng.generate_range(8, 9);
+            let next = rng.generate_range(0, 7);
             leds[next].on().ok();
             delay.delay_ms(half_period);
             leds[next].off().ok();

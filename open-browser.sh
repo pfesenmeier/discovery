33776b1fe31fcd781a4c10a6af2e66fb3bfeecc0
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-06-29 21:05:12 -0400

    Add script to work with cargo doc in WSL

---
+++ open-browser.sh
@@ -0,0 +1,10 @@
+# open_browser.sh
+#! /bin/bash
+#echo $1
+winPath="file://///wsl$//Ubuntu${1}"
+#echo ${winPath}
+#firefox
+#/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe $winPath
+#chrome
+/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe $winPath
+

e6b90ca14c5cfc450560770a2fa0cbb5d20d4082
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-07-15 22:44:10 -0400

    Finish clocks and timers

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -4,11 +4,19 @@
 use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
-fn delay(_tim6: &tim6::RegisterBlock, ms: u16) {
-    const K: u16 = 3; // this value needs to be tweaked
-    for _ in 0..(K * ms) {
-        aux9::nop()
-    }
+fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
+    // Set the timer to go off in `ms` ticks
+    // 1 tick = 1 ms
+    tim6.arr.write(|w| w.arr().bits(ms));
+
+    // CEN: Enable (and start) the counter
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+
+    // Wait until the alarm goes off (until the update event occurs)
+    while !tim6.sr.read().uif().bit_is_set() {}
+
+    // Clear the update event flag
+    tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
 #[entry]
@@ -16,18 +24,24 @@ fn main() -> ! {
     let (leds, rcc, tim6) = aux9::init();
     let mut leds = leds.into_array();
 
-    // TODO initialize TIM6
+    // initialize TIM6
     rcc.apb1enr.modify(|_, w| w.tim6en().set_bit());
 
-    let ms = 999;
+    // OPM Select one pulse mode
+    // CEN Keep the counter disabled for now
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+
+    // Configure the prescaler to have the counter operate at 1 KHz
+    tim6.psc.write(|w| w.psc().bits(7_999));
+
     loop {
         for curr in 0..8 {
             let next = (curr + 1) % 8;
 
             leds[next].on().unwrap();
-            delay(tim6, ms);
+            delay(tim6, 100);
             leds[curr].off().unwrap();
-            delay(tim6, ms);
+            delay(tim6, 50);
         }
     }
 }

83b4e9ca86944e1a8338ad2d1801638b3b622b23
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-07-21 22:46:23 -0400

    Implement uprint macro

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -1,17 +1,52 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        // TODO implement this
+        // hint: this will look very similar to the previous program
+        for byte in s.as_bytes().into_iter() {
+            // wait until it's safe to write to TDR
+            while self.usart1.isr.read().txe().bit_is_clear() {} // <- NEW!
+
+            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    let mut serial = SerialPort { usart1 };
+
+    uprintln!(serial, "The answer is {}", 40 + 2);
 
     loop {}
 }

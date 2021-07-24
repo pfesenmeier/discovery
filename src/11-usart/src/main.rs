c745546af3ca05b3e7892b401dbfff88ac729445
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-07-24 13:25:41 -0400

    first pass reversing string

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -4,20 +4,44 @@
 
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln};
+use heapless::Vec;
 
 #[entry]
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
     loop {
+        buffer.clear();
+
+        // TODO Receive a user request. Each user request ends with ENTER
         // Wait until there's data available
         while usart1.isr.read().rxne().bit_is_clear() {}
 
         // Retrieve the data
         let byte = usart1.rdr.read().rdr().bits() as u8;
+        // if byte != '\n'
+        if byte == 10 {
+          print_bytes(&buffer)
+        } else {
+           
+        }
+        // push on buffer
+        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
+        // with an error message.
 
-        // Send the data
-        usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)))
+        /*
+          reverse string
+          print out string byte by byte
+         */
+        // TODO Send back the reversed string
     }
 }
+
+fn print_bytes(_bytes: &Vec<u8, 32>) -> () {
+
+}
+
 

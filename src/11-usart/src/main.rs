4770714576163e3b02bea210cdda2d835cde2632
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-07-24 15:36:45 -0400

    Reverse a string

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -8,40 +8,40 @@ use heapless::Vec;
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, _mono_timer, mut itm) = aux11::init();
 
     // A buffer with 32 bytes of capacity
     let mut buffer: Vec<u8, 32> = Vec::new();
 
     loop {
-        buffer.clear();
-
         // TODO Receive a user request. Each user request ends with ENTER
         // Wait until there's data available
         while usart1.isr.read().rxne().bit_is_clear() {}
 
         // Retrieve the data
         let byte = usart1.rdr.read().rdr().bits() as u8;
-        // if byte != '\n'
-        if byte == 10 {
-          print_bytes(&buffer)
-        } else {
-           
+        match byte {
+            // '\r'
+            13 => {
+                buffer.reverse();
+                for byte in &buffer {
+                    while usart1.isr.read().txe().bit_is_clear() {}
+                    usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+                }
+                buffer.clear();
+            }
+            _ => {
+                if let Err(x) = buffer.push(byte) {
+                    iprintln!(&mut itm.stim[0], "Error while pushing '{}' to array", x);
+                }
+            }
         }
-        // push on buffer
-        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
-        // with an error message.
-
-        /*
-          reverse string
-          print out string byte by byte
-         */
-        // TODO Send back the reversed string
     }
-}
-
-fn print_bytes(_bytes: &Vec<u8, 32>) -> () {
-
+    /*
+      reverse string
+      print out string byte by byte
+     */
+    // TODO Send back the reversed string
 }
 
 
